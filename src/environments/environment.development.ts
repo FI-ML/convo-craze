80a437ba0b47d5ba458bea7422933a821cde1c3a
FI-ML <maximilianlamm.kontakt@outlook.de>
2023-05-27 12:39:14 +0200

    Updates

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-import {Env} from "./env";
-
-export const environment = {
-  production: false,
-  apiKey: Env.API_KEY,
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-import {Env} from "./env";
-
-export const environment = {
-  production: true,
-  apiKey: Env.API_KEY,
-};
